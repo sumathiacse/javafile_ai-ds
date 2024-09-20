173e07af2c290a2d72c53304d702a5e29e595e66
sumathiacse <133768248+sumathiacse@users.noreply.github.com>
2024-09-20 14:35:57 +0530

    changed in github

--- test.java
+++ test.java
@@ -5,6 +5,5 @@ public static void main(String args[])
 for(int i=1;i<=5;i++)
 System.out.println("Welcome to jenkins");
 System.out.println("Welcome to world!!!!");
-System.out.println("Welcome all!!!!");
 }
 }
